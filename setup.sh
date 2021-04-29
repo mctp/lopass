85b6aa9bf720d5da30d8d738601615604aa1b173
Marcin Cieslik <mcieslik@med.umich.edu>
2021-04-29 03:49:52 +0000

    basic setup code

---
+++ setup.sh
@@ -0,0 +1,5 @@
+__conda_setup="$('/opt/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+eval "$__conda_setup"
+conda config --add channels bioconda
+conda config --add channels conda-forge
+sudo conda env create -n lopass --file conda-lopass.yml
