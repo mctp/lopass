e8ae0c1be4c6e105e9fcc3df5bce5e46f0c3b6d4
Marcin Cieslik <mcieslik@med.umich.edu>
2021-05-01 19:05:22 +0000

    conda setup script

--- setup-conda.sh
+++ setup-conda.sh
@@ -1,5 +1,8 @@
-__conda_setup="$('/opt/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-eval "$__conda_setup"
+#!/usr/bin/env bash
+
+## setup conda
+# __conda_setup="$('/opt/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+# eval "$__conda_setup"
 conda config --add channels bioconda
 conda config --add channels conda-forge
 sudo conda env create -n lopass --file conda-lopass.yml
