bd665d901048711a7e588c4aee9d0293e3a31b06
Marcin Cieslik <mcieslik@med.umich.edu>
2021-05-02 03:40:19 +0000

    fix gatk calling intervals and organize

---
+++ data/external/readme.txt
@@ -0,0 +1,2 @@
+GATK resources bundle:
+gs://genomics-public-data/resources/broad/hg38/v0/wgs_calling_regions.hg38.interval_list
--- scripts/gatk-prep-intervals.R
+++ scripts/gatk-prep-intervals.R
@@ -5,7 +5,7 @@ library(GenomeInfoDb)
 library(BSgenome.Hsapiens.UCSC.hg38)
 library(rtracklayer)
 
-tmp <- fread("data/wgs_calling_regions.hg38.interval_list", skip = 3368)
+tmp <- fread("data/external/wgs_calling_regions.hg38.interval_list", skip = 3368)
 rng.call <- with(tmp, GRanges(V1, IRanges(V2, V3), V4))
 
 gseqi <- seqinfo(BSgenome.Hsapiens.UCSC.hg38)
@@ -33,7 +33,8 @@ par2.beg <- GRanges("chrX", IRanges(155701383, 155701383), "+")
 par1.split <- which(rng %over% par1.end)
 end(rng[par1.split-1]) <- 2781479
 start(rng[par1.split]) <- 2781479+1
-rng[1:(par1.split-1)]$xy.ploidy <- 1
+firstX <- min(which(seqnames(rng)=="chrX"))
+rng[firstX:(par1.split-1)]$xy.ploidy <- 1
 
 par2.split <- which(rng %over% par2.beg)
 end(rng[par2.split-1]) <- 155701383-1
@@ -41,4 +42,4 @@ start(rng[par2.split]) <- 155701383
 rng[par2.split:last(which(seqnames(rng)=="chrX"))]$xy.ploidy <- 1
 
 rng.tbl <- as.data.table(rng)[,.(chr=seqnames, start, end, xx.ploidy, xy.ploidy)]
-fwrite(rng.tbl, "data/wgs_calling_full_regions.hg38.intervals", sep="\t", col.names=FALSE)
+fwrite(rng.tbl, "./hg38/intervals/wgs-npartition-ploidy.tsv", sep="\t", col.names=FALSE)
